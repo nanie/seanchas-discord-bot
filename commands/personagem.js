0ac657540ab7cfa96cc4ff703726db807c2b4832
Camilla-mac <camillacamatta@gmail.com>
2020-12-23 15:26:51 -0300

    character creation

---
+++ commands/personagem.js
@@ -0,0 +1,21 @@
+module.exports =
+{
+    name: 'personagem',
+    description: 'Mostra conteudo da sacola',
+    execute(message, currentBag)
+    {
+        const Util = require('../seanchasUtils');
+        const seasons = Util.GetSeasons();
+        const descriptions = Util.GetDescriptions();
+        const attributes = Util.GetCharacterAttributes();
+        var text = '';
+
+        for (j = 0; j < 3; j++) {
+            const randomIndex = Math.floor(Math.random() * seasons.length); 
+            selected += attributes[j] + ': ' + seasons[randomIndex] + ' - ' + descriptions[randomIndex] + '\n';
+        }
+       
+        
+        message.channel.send(text);
+    }
+}
--- seanchasUtils.js
+++ seanchasUtils.js
@@ -1,7 +1,7 @@
 
 function CreateBag() {
     var bag = [];
-    const seasons = [':sunny:', ':maple_leaf:', ':snowflake:', ':sunflower:'];
+    const seasons = GetSeasons();
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 4; j++) {
             bag.push(seasons[j]);
@@ -10,4 +10,19 @@ function CreateBag() {
     return bag;
 }
 
-module.exports = { CreateBag };
+function GetSeasons()
+{
+    return [':sunny:', ':maple_leaf:', ':snowflake:', ':sunflower:'];
+}
+
+function GetDescriptions()
+{
+    return ['Humano, razão, corpo','Selvagem, Bestial, Irracional','Magia destrutiva, da morte','Magia criativa, da vida'];
+}
+
+function GetCharacterAttributes()
+{
+    return ['Origem','Motivação','Destino'];
+}
+
+module.exports = { CreateBag, GetSeasons, GetDescriptions, GetCharacterAttributes};
