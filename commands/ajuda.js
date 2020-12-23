c22619ad1b7a53b0b4874ec63f645f083ad719d0
Camilla-mac <camillacamatta@gmail.com>
2020-12-23 15:33:50 -0300

    Update ajuda.js

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -4,7 +4,7 @@ module.exports =
     description: 'Mostra conteudo da sacola',
     execute(message, currentBag)
     {
-        var text = 'Use o comando $sacola para ver o que tem na sacola, $pegar para pegar 4 marcadores e $reiniciar para retornar todos os marcadores para a sacola';
+        var text = 'Use o comando $sacola para ver o que tem na sacola,$personagem para criar um personagem aleatório, $pegar para pegar 4 marcadores e $reiniciar para retornar todos os marcadores para a sacola';
         message.channel.send(text);
         text = 'Marcadores: Verão - :sunny: Outono - :maple_leaf: Inverno - :snowflake: Primavera - :sunflower:';
         message.channel.send(text);
